8104923cd647e9df79b8aecf8bbeda38ef8bb1d3
msnyde8@students.towson.edu <msnyde8@students.towson.edu>
2015-03-02 13:25:38 -0500

    Updates to Project4 from work ... lunch break! :)

--- src/project_4/Project4.java
+++ src/project_4/Project4.java
@@ -1,33 +1,43 @@
 package project_4;
 
-import java.io.IOException;
 import java.util.LinkedList;
+import java.util.ListIterator;
+import java.util.NoSuchElementException;
 import java.util.Scanner;
 
 public class Project4 {
+	
+	static LinkedList<String> storedData = new LinkedList<String>();
+	static Scanner lineReader;
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-//		LinkedList storedData = new LinkedList();
 		String textStr = "";
-		Scanner reader = new Scanner(System.in);
+		lineReader = new Scanner(System.in);
 		Boolean end = false;
-		/*try
-		{*/
+		try
+		{
+			Boolean newCommand = true;
 			System.out.println("Please enter a command or a line of text");
-			while((false == end) && reader.hasNextLine())
+			
+			do
 			{
-				textStr = reader.nextLine();
+				textStr = lineReader.nextLine();
+				
 				switch(textStr)
 				{
 					case "$insert":
 						System.out.println("Insert statement found!");
+						insertStatement();
+						newCommand = false;
 						break;
 					case "$delete":
 						System.out.println("Delete statement found!");
 						break;
 					case "$print":
 						System.out.println("Print statement found!");
+						printStatement();
+						newCommand = true;
 						break;
 					case "$line":
 						System.out.println("Line statement found!");
@@ -38,21 +48,86 @@ public class Project4 {
 					case "$done":
 						System.out.println("Thank you and have a nice day!");
 						end = true;
-						reader.close();
+						lineReader.close();
+						newCommand = false;
 						break;
 					default:
 						System.out.println("Unuseful!!");
 						break;
 				}
 				
-			}
-			/*
+				if(true == newCommand)
+				{
+					System.out.println("Please enter a command or a line of text");
+				}
+			}while((false == end) && lineReader.hasNextLine());
 		}
-		catch(IOException e)
+		catch(IllegalStateException e)
 		{
-			System.out.println("Caught IOException: " + e.getMessage());
-        	e.printStackTrace();
+			System.out.println("Caught IllegalStateException: " + e.getMessage());
+			e.printStackTrace();
 		}
-	*/
+	}
+	
+	static void insertStatement()
+	{
+		try
+		{
+			while((!lineReader.hasNext("\\$delete")) && 
+					(!lineReader.hasNext("\\$print")) &&
+					(!lineReader.hasNext("\\$line")) &&
+					(!lineReader.hasNext("\\$search")) &&
+					(!lineReader.hasNext("\\$done")))
+			{
+				storedData.add(lineReader.nextLine());
+			}
+		}
+		catch(IllegalStateException e)
+		{
+			System.out.println("Caught IllegalStateException: " + e.getMessage());
+			e.printStackTrace();
+		}
+		catch(NoSuchElementException e)
+		{
+			System.out.println("Caught NoSuchElementException: " + e.getMessage());
+			e.printStackTrace();
+		}
+		catch(UnsupportedOperationException e)
+		{
+			System.out.println("Caught UnsupportedOperationException: " + e.getMessage());
+			e.printStackTrace();
+		}
+		catch(ClassCastException e)
+		{
+			System.out.println("Caught ClassCastException: " + e.getMessage());
+			e.printStackTrace();
+		}
+		catch(IllegalArgumentException e)
+		{
+			System.out.println("Caught IllegalArgumentException: " + e.getMessage());
+			e.printStackTrace();
+		}
+	}
+	
+	static void printStatement()
+	{
+		try
+		{
+			ListIterator<String> dataIterator = storedData.listIterator();
+			while(dataIterator.hasNext())
+			{
+				System.out.println(dataIterator.next());
+			}
+		}
+		catch(IllegalStateException e)
+		{
+			System.out.println("Caught IllegatlStateException: " + e.getMessage());
+			e.printStackTrace();
+		}
+		catch(NoSuchElementException e)
+		{
+			System.out.println("Caught NoSuchElementException: " + e.getMessage());
+			e.printStackTrace();
+		}
 	}
 }
