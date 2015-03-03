90f4ea75c1dcfb0158c3d1908452edc8a3bca7e4
msnyde8@students.towson.edu <msnyde8@students.towson.edu>
2015-03-03 11:38:26 -0500

    Updated answer tests for project_3

--- src/project_3/Project3.java
+++ src/project_3/Project3.java
@@ -6,8 +6,49 @@ public class Project3 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		System.out.println("Problem 6: ");
+		problem_6();
+		System.out.println("Problem 7 - (2,8): ");
+		problem_7(2,8);
+		//System.out.println("Problem 7 - (5,6): ");
+		//problem_7(5,6);
+		//System.out.println("Problem 7 - (7,5): ");
+		//problem_7(7,5);
+		System.out.println("Problem 8: ");
 		problem_8();
-		problem_9();
+		System.out.println("Problem 9: ");
+		System.out.println("Part 1: ");
+		problem_9_part1();
+		System.out.println("Part 2: ");
+		problem_9_part2();
+	}
+	
+	static void problem_6()
+	{
+		System.out.println(function(5));
+	}
+	static int function(int n)
+	{
+		if (n == 0 || n == 1)
+		{
+			return 1;
+		}
+		else
+		{
+			return 2*function(n-2) + 3*function(n-1);
+		}
+	}
+	
+	static void problem_7(int p1, int p2)
+	{
+		if (p1 != p2) 
+		{
+			p1 = p1 + 2;
+			p2 = p2 - 1;
+			problem_7(p1,p2);
+			System.out.println(p1);
+			System.out.println(p2);
+		}
 	}
 	
 	static void problem_8()
@@ -22,7 +63,12 @@ public class Project3 {
         }
 	}
 
-	static void problem_9()
+	static void problem_9_part1()
+	{
+		
+	}
+
+	static void problem_9_part2()
 	{
 		
 	}
